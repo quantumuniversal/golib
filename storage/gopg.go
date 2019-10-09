3406ef62afdd7596ea9b9db2b73e2ee233d87cd2
Raph <raph@hcg.com.qa>
2019-10-09 09:34:37 +0300

    add query

--- storage/gopg.go
+++ storage/gopg.go
@@ -13,12 +13,61 @@ type Gopg struct {
 	Auth      pg.Options
 	Schema    string
 	TableName string
+	Query     Query
 	Model     interface{}
 	Data      interface{}
 }
 
-// SelectMany --
-func (g *Gopg) SelectMany() error {
+// Query --
+type Query struct {
+	Column     string
+	ColumnExpr string // count(*) as column_count
+	Where      []Where
+	WhereOr    []Where
+	Order      string
+	Limit      int
+}
+
+// Where --
+type Where struct {
+	Condition string
+	Param     interface{}
+}
+
+func (g *Gopg) prepareStatement(q *orm.Query) *orm.Query {
+	if g.Query.Column != "" {
+		q = q.Column(g.Query.Column)
+	}
+
+	if g.Query.ColumnExpr != "" {
+		q = q.ColumnExpr(g.Query.ColumnExpr)
+	}
+
+	if len(g.Query.Where) > 0 {
+		for _, statement := range g.Query.Where {
+			q = q.Where(statement.Condition, statement.Param)
+		}
+	}
+
+	if len(g.Query.WhereOr) > 0 {
+		for _, statement := range g.Query.WhereOr {
+			q = q.WhereOr(statement.Condition, statement.Param)
+		}
+	}
+
+	if g.Query.Order != "" {
+		q = q.Order(g.Query.Order)
+	}
+
+	if g.Query.Limit != 0 {
+		q = q.Limit(g.Query.Limit)
+	}
+
+	return q
+}
+
+// Select --
+func (g *Gopg) Select() error {
 	db := pg.Connect(&g.Auth)
 	defer db.Close()
 
@@ -36,7 +85,10 @@ func (g *Gopg) SelectMany() error {
 		Temp: false,
 	})
 
-	err = db.Model(g.Data).Select()
+	q := g.prepareStatement(db.Model(g.Data))
+
+	err = q.Select()
+
 	if err != nil {
 		return err
 	}
@@ -44,15 +96,15 @@ func (g *Gopg) SelectMany() error {
 	return nil
 }
 
-// SelectWhere --
-func (g *Gopg) SelectWhere(condition string, param interface{}) error {
+// SelectCount --
+func (g *Gopg) SelectCount() (int, error) {
 	db := pg.Connect(&g.Auth)
 	defer db.Close()
 
 	_, err := db.Exec(fmt.Sprintf("CREATE Schema IF NOT EXISTS \"%s\";", g.Schema))
 
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	orm.SetTableNameInflector(func(s string) string {
@@ -63,12 +115,15 @@ func (g *Gopg) SelectWhere(condition string, param interface{}) error {
 		Temp: false,
 	})
 
-	err = db.Model(g.Data).Where(condition, param).Select()
+	q := g.prepareStatement(db.Model(g.Data))
+
+	count, err := q.Count()
+
 	if err != nil {
-		return err
+		return 0, err
 	}
 
-	return nil
+	return count, nil
 }
 
 // Insert --
